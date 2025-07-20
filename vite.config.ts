372560434b345c43514e7f05acf86351c2406858
Rohit_Kumar_Routhu <rohit.kumar@nextuple.com>
2025-07-20 13:59:01 +0530

    fixed the issues on 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/Meghamsh-Portfolio.github.io/', // needed for GitHub Pages
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: '/Meghamsh-Portfolio.github.io/',
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // helpful for imports
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  server: {
-    host: "::",
-    port: 8080,
-  }
-}));
+});
