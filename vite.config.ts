df39a827a100711a8bb19870afe47e9daeff5e89
meghamsh-r <mrouthumeghamsh@gmail.com>
2025-08-04 17:35:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-   base: '/Meghamsh-Portfolio.github.io/', // needed for GitHub Pages
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
